5e6ad3ee6a52f4e8898d6c75dcb1e8eee7cabf21
1AshishJadhav <aashishjadhav124@gmail.com>
2024-09-02 15:20:17 +0530

    First Commit

--- login.py
+++ login.py
@@ -17,11 +17,22 @@ frame.place(x=480, y=70)
 heading = Label(frame, text='Sign In', fg='#57a1f8', bg='white', font=('Microsoft YaHei UI Light', 23, 'bold'))
 heading.place(x=100, y=5)
 
+
+###############----------------------------------------------
+
 user = Entry(frame, width=25, fg='black', border=0, bg='white', font=('Microsoft YaHei UI Light', 11))
 user.place(x=40, y=80)
 user.insert(0, 'Username')
 
 Frame(frame, width=295, height=2, bg='black').place(x=25, y=107)
+
+#########-------------------------------------------------
+
+code = Entry(frame, width=25, fg='black', border=0, bg='white', font=('Microsoft YaHei UI Light', 11))
+code.place(x=40, y=150)
+code.insert(0, 'Password')
+
+Frame(frame, width=295, height=2, bg='black').place(x=25, y=177)
 
 
             
